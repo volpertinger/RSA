635c52968c79903b709756f9b95586d71397d0a4
Nikolay <merzlovnik@mail.ru>
2023-04-07 14:44:37 +0300

    json Settings

---
+++ KeyGenerator/DTO.cs
@@ -0,0 +1,9 @@
+﻿namespace KeyGenerator
+{
+    public class Settings
+    {
+        public ulong minPrimeValue { get; set; }
+        public ulong maxPrimeValue { get; set; }
+        public int seed { get; set; }
+    }
+}
--- KeyGenerator/Program.cs
+++ KeyGenerator/Program.cs
@@ -1,4 +1,11 @@
-﻿class Program
+﻿using NLog.Config;
+using NLog;
+using KeyGenerator;
+using NLog.Targets;
+using System.Reflection;
+using System.Text.Json;
+
+class Program
 {
     private static string logo = @"
 ██████╗ ███████╗ █████╗     ██╗  ██╗███████╗██╗   ██╗                                                                 
@@ -24,9 +31,43 @@
                                                                                                                                    
                                                                                                                                       
 ";
+
+    private static LoggingConfiguration config = new LoggingConfiguration();
+    private static Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+    private static int Success = 0;
+    private static int SettingsError = 1;
+    private static int JsonFormatError = 2;
     static int Main(string[] args)
     {
+        // logger settings
+        config.AddRule(LogLevel.Debug, LogLevel.Fatal, new ConsoleTarget
+        {
+            Name = "console",
+            Layout = "${longdate}|${level:uppercase=true}|${logger}|${message}",
+        }, "*");
+        LogManager.Configuration = config;
+
         Console.WriteLine(logo);
-        return 0;
+        Settings? settings = null;
+        string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? "", @"Settings.json");
+        try
+        {
+            string jsonString = File.ReadAllText(path);
+            settings = JsonSerializer.Deserialize<Settings>(jsonString);
+        }
+        catch
+        {
+            logger.Error(String.Format("Can`t find Settings.json at path {0}", path));
+            return SettingsError;
+        }
+
+        if (settings is null)
+        {
+            logger.Error("Invalid Settings.json file. Check Readme!");
+            return JsonFormatError;
+        }
+
+        return Success;
     }
 }
